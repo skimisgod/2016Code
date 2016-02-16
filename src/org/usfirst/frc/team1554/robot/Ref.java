e9a9b00d80acc864b24675614dcdb6f747bea791
Matthew Crocco <matthewcrocco@gmail.com>
2016-02-16 16:39:11 -0500

    I fixed your code. Be happy. ;)

--- src/org/usfirst/frc/team1554/robot/Ref.java
+++ src/org/usfirst/frc/team1554/robot/Ref.java
@@ -1,14 +1,9 @@
 package org.usfirst.frc.team1554.robot;
 
 public class Ref {
-	
-	public static final int Y_MAX = 1;
-    public static final int X_MAX = 1;
-    public static final int Z_MAX = 1;
-    public static final int Y_MIN = -1;
-    public static final int X_MIN = -1;
-    public static final int Z_MIN = -1;
+
     public static final int LIFE_BUTTON = 12;
-    public static final int CROCCO_CHANNEL = 2;
-    
+    public static final int TALON_LEFT = 0, TALON_RIGHT = 1;
+    public static final int JOYSTICK_LEFT = 0, JOYSTICK_RIGHT = 1;
+
 }
--- src/org/usfirst/frc/team1554/robot/Robot.java
+++ src/org/usfirst/frc/team1554/robot/Robot.java
@@ -1,14 +1,6 @@
 
 package org.usfirst.frc.team1554.robot;
 
-import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.TalonSRX;
-
-
-
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the IterativeRobot
@@ -16,161 +8,91 @@ import edu.wpi.first.wpilibj.TalonSRX;
  * creating this project, you must also update the manifest file in the resource
  * directory.
  */
-import java.lang.Math;
+import org.usfirst.frc.team1554.lib.common.control.AutoJoystickUpdater;
+import org.usfirst.frc.team1554.lib.common.control.DualJoystickControl;
+import org.usfirst.frc.team1554.lib.common.control.Hand;
+import org.usfirst.frc.team1554.lib.common.control.JoystickControl;
+import org.usfirst.frc.team1554.lib.common.robot.EnhancedIterativeRobot;
+import org.usfirst.frc.team1554.lib.common.system.BasicSense;
+import org.usfirst.frc.team1554.lib.common.system.MotorScheme;
+import org.usfirst.frc.team1554.lib.common.system.MotorScheme.DriveManager;
 
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.Talon;
 
+public class Robot extends EnhancedIterativeRobot {
 
+    private final Talon leftTread, rightTread;
+    private final MotorScheme scheme;
+    private final BasicSense senses;
+    private final JoystickControl joystick;
+    private final AutoJoystickUpdater updater;
 
+    public Robot() {
+        super("Sailors", 1554);
+        leftTread = new Talon(Ref.TALON_LEFT);
+        rightTread = new Talon(Ref.TALON_RIGHT);
 
-
-public class Robot extends IterativeRobot {
-	RobotDrive myRobot; //Controls the drive machine
-    Joystick rightStick; //Joy stick that is meant for movement of right tread 
-    Joystick leftStick; //Joy stick that is meant for movement of the left tread
-    Move move = new Move(); //Constructor for the move class
-    boolean alive = false; //boolean alive stores the value of isEnabled()
-    TalonSRX crocco;
-    
-    int x = 0;
-	
-	
-	
-	
-    final String defaultAuto = "Default";
-    final String customAuto = "My Auto";
-    String autoSelected;
-    SendableChooser chooser;
-	
-    /**
-     * This function is run when the robot is first started up and should be
-     * used for any initialization code.
-     */
-   
-   public void robotInit() {
-	   	myRobot = new RobotDrive(0,1); //sets left motor channel to 0, right motor channel to 1
-       	leftStick = new Joystick(1); //sets USB port to USB port 1
-       	rightStick = new Joystick(2); //sets USB port to USB port 2 
-    	crocco = new TalonSRX(Ref.CROCCO_CHANNEL);
-    	crocco.setSafetyEnabled(true);
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	chooser = new SendableChooser();
-        chooser.addDefault("Default Auto", defaultAuto);
-        chooser.addObject("My Auto", customAuto);
-        SmartDashboard.putData("Auto choices", chooser);
-    }
-    
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
-	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
-	 * Dashboard, remove all of the chooser code and uncomment the getString line to get the auto name from the text box
-	 * below the Gyro
-	 *
-	 * You can add additional auto modes by adding additional comparisons to the switch structure below with additional strings.
-	 * If using the SendableChooser make sure to add them to the chooser code above as well.
-	 */
-    public void autonomousInit() {
-    	 alive = isEnabled();
-    	 // autonomous notifier
-    	System.out.println("Autonomus start");
-    	
-    	
-    	
-    	autoSelected = (String) chooser.getSelected();
-//		autoSelected = SmartDashboard.getString("Auto Selector", defaultAuto);
-		System.out.println("Auto selected: " + autoSelected);
+        senses = BasicSense.makeBuiltInSense();
+        scheme = MotorScheme.Builder.newTwoChannelDrive(leftTread, rightTread).build();
+        scheme.setDriveManagement(DriveManager.TANK);
+        joystick = new DualJoystickControl(Ref.JOYSTICK_LEFT, Ref.JOYSTICK_RIGHT);
+        updater = new AutoJoystickUpdater(joystick);
     }
 
-    /**
-     * This function is called periodically during autonomous
-     */
-    
-    public void autonomousPeriodic() {
-    	 myRobot.setSafetyEnabled(false);
-         while(isAutonomous() && isEnabled()){
-         }
-    	
-    	
-    	switch(autoSelected) {
-    	case customAuto:
-        //Put custom auto code here   
-            break;
-    	case defaultAuto:
-    	default:
-    	//Put default auto code here
-            break;
-    	}
-    }
-    public void teleopInit(){ //Called once each time the robot enter teleop
-        alive = isEnabled(); //alive stores true if the robot is enabled
+    @Override protected void onInitialization() {
+        joystick.putButtonAction(Ref.LIFE_BUTTON, "Life Button", this::killMotors, Hand.RIGHT);
     }
 
-    /**
-     * This function is called periodically during operator control
-     */
-    public void teleopPeriodic() {
-    	myRobot.setSafetyEnabled(true);
-        while(alive && isOperatorControl()) //isOperatorControl will be true if robot is not in autonomous or test mode
-        {
-        	
-        	myRobot.tankDrive(-leftStick.getY(), -rightStick.getY()); //Drive method (can change to double values using getY()
-            if(Math.abs(rightStick.getTwist()) == Ref.Z_MAX){ // only uses precise turn if twist is at its max value
-                // precise turn notifier
-            	System.out.println("precise turn");
-            	move.preciseTurn(rightStick,myRobot);
-            }
-            alive = move.deathSwitch(rightStick);//will turn drive train off
-             
-        }
-        
-        alive = move.lifeSwitch(rightStick);//will turn drive train on 
+    private void killMotors() {
+        // DOES STUFF
     }
-    
-    /**
-     * This function is called periodically during test mode
-     */
-    public void testPeriodic() {
-    	 while(isTest() && isEnabled()){
-             LiveWindow.run();
-             // Joystick data
-             double lx = Math.round(leftStick.getX() * 1000) / 1000;
-             double ly = Math.round(leftStick.getY() * 1000) / 1000;
-             double lz = Math.round(leftStick.getZ() * 1000) / 1000;
-             double rx = Math.round(rightStick.getX() * 1000) / 1000;
-             double ry = Math.round(rightStick.getY() * 1000) / 1000;
-             double rz = Math.round(rightStick.getZ() * 1000) / 1000;
-             /*if(Math.abs(rightStick.getX()) > .01)
-            	 System.out.println("X: " + rightStick.getX());
-             if(Math.abs(rightStick.getY()) > .01)
-            	 System.out.println("Y: " + rightStick.getY());
-             if(Math.abs(rightStick.getZ()) > .01)
-            	 System.out.println("Z: " + rightStick.getZ());
-             if(leftStick.getRawButton(1))
-            	 System.out.println("FIRE!");*/
-             
-             System.out.print("LX: " + lx + " LY: " + ly + " LZ: " + lz
-            		 + " RX: " + rx + " RY: " + ry + " RZ: " + rz + "\r");
-             Timer.delay(.5); 
-             /*if (x < 1) {
-            	 move.turnOn(crocco);
-                 Timer.delay(1.0);
-                 move.turnOff(crocco);
-                 x = 10;
-             }*/
-             
-             
-    	 }   
-    
+
+    @Override public void dispose() {
+        updater.stop();
     }
-    
+
+    @Override public void preDisabled() {
+
+    }
+
+    @Override public void onDisabled() {
+
+    }
+
+    @Override public void preAutonomous() {
+
+    }
+
+    @Override public void onAutonomous() {
+
+    }
+
+    @Override public void preTeleop() {
+
+    }
+
+    @Override public void onTeleop() {
+        this.updateDrive();
+    }
+
+    @Override public void onTest() {
+
+    }
+
+    @Override public RobotDrive getDrive() {
+        return scheme.getRobotDrive();
+    }
+
+    @Override public JoystickControl getJoysticks() {
+        return joystick;
+    }
+
+    @Override public MotorScheme getMotorScheme() {
+        return scheme;
+    }
+
+    @Override public BasicSense getBasicSenses() {
+        return senses;
+    }
 }
