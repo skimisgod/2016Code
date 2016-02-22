fac8b5c2def5c78f0af7eb8f7cf7a3c157bb3ff2
skimisgod <jazzag@optonline.net>
2016-02-22 15:56:49 -0500

    Updated stuff? Please help, sir.

--- src/org/usfirst/frc/team1554/robot/Ref.java
+++ src/org/usfirst/frc/team1554/robot/Ref.java
@@ -6,10 +6,11 @@ public class Ref {
     public static final int TALON_LEFT = 0, TALON_RIGHT = 1;
     public static final int JOYSTICK_LEFT = 0, JOYSTICK_RIGHT = 1;
     public static final int FEEDER_CHANNEL = 2;
-    public static final int FEED_BUTTON = 3;
+    public static final int FEED_BUTTON = 2;
     public static final int PIVOT_CHANNEL = 3;
     public static final int PIVOT_BUTTON = 3;
     public static final int TRIGGER_CHANNEL = 4;
+    public static final int TRIGGER = 1;
     
 
 }
